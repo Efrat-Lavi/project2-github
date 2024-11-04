c16e919ffb34da3d51fdd68a98b587bdb59c3fd9
Efrat_Lavi <efratl3164@gmail.com>
2024-11-04 22:55:34 +0200

    delete trying

--- Netivot_Project/Netivot_Project/Enums/ActiveStatusEnum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Netivot_Project.Enums
-{
-    public enum ActiveStatusEnum
-    {
-        Complete = 0,
-        Active = 1
-    }
-}
--- Netivot_Project/Netivot_Project/Enums/GenderEnum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Netivot_Project.Enums
-{
-    public enum GenderEnum
-    {
-        male = 1,
-        female = 2
-    }
-}
